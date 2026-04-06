bca805d467c98c8b5474d3a64e032c520723c56b
Atreya Kamat <atkamat1204@gmail.com>
2026-04-06 11:56:55 +0530

    feat: migrate from @studio-freight/react-lenis to lenis and clean up package dependencies

--- src/components/SmoothScroll.tsx
+++ src/components/SmoothScroll.tsx
@@ -1,12 +1,11 @@
 "use client";
-import { ReactLenis } from "@studio-freight/react-lenis";
+import { ReactLenis } from "lenis/react";
 import { ReactNode } from "react";
 
 export default function SmoothScroll({ children }: { children: ReactNode }) {
   return (
     <ReactLenis root options={{ lerp: 0.05, duration: 1.5, smoothWheel: true }}>
-      {/* eslint-disable-next-line @typescript-eslint/no-explicit-any */}
-      {children as any}
+      {children}
     </ReactLenis>
   );
 }
